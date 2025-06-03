1e73d4ef5ada3ec40f3790d1a9ec392cc7db64d2
FGFAnderson <fgfanderson@gmail.com>
2025-06-03 11:45:03 +0100

    fix: Fix player_team_history errors

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,6 +4,7 @@ from models.competition import Competition
 from models.team_competition import TeamCompetition
 from models.team import Team
 from models.match import Match
+from models.player import Player
+from models.player_team_history import PlayerTeamHistory
 
-
-__all__ = ["Base", "Competition", "Match", "Organisation", "TeamCompetition", "Team"]
+__all__ = ["Base", "Competition", "Match", "Organisation", "TeamCompetition", "Team", "Player", "PlayerTeamHistory"]
--- src/models/player_team_history.py
+++ src/models/player_team_history.py
@@ -1,8 +1,12 @@
 from datetime import datetime
+from typing import TYPE_CHECKING, Optional
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import DateTime, ForeignKey
 from models.base import BaseModel
 
+if TYPE_CHECKING:
+    from models.team import Team
+    from models.player import Player
 
 class PlayerTeamHistory(BaseModel):
     __tablename__ = "player_team_history"
@@ -10,9 +14,9 @@ class PlayerTeamHistory(BaseModel):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     player_id: Mapped[int] = mapped_column(ForeignKey("players.id"))
-    team_id: Mapped[str] = mapped_column(ForeignKey("teams.id"))
+    team_id: Mapped[int] = mapped_column(ForeignKey("teams.id"))
     joined_at: Mapped[datetime] = mapped_column(DateTime())
-    left_at = Mapped[datetime] = mapped_column()
+    left_at: Mapped[Optional[datetime]] = mapped_column(DateTime())
 
-    user = relationship("User", back_populates="team_memberships")
-    team = relationship("Team", back_populates="user_memberships")
+    player: Mapped["Player"] = relationship(back_populates="team_history")
+    team: Mapped["Team"] = relationship(back_populates="teams")
